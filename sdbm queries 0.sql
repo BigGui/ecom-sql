7e5577ade148dc0760f66f3f3523f7f61ea7749c
Guillaume Belleuvre <guillaumebelleuvre@gmail.com>
2024-06-11 14:42:13 +0200

    sdbm queries 0 add numbers to questions

--- sdbm queries 0.sql
+++ sdbm queries 0.sql
@@ -1,27 +1,27 @@
--- Combien de bières différentes sont disponibles dans la base de données ?
+-- 1/ Combien de bières différentes sont disponibles dans la base de données ?
 
 
 
--- Quel est le nom de la bière avec le prix de vente le plus élevé ?
+-- 2/ Quel est le nom de la bière avec le prix de vente le plus élevé ?
 
 
 
--- Quel est le nom du continent qui compte le plus grand nombre de pays répertoriés dans la base de données ?
+-- 3/ Quel est le nom du continent qui compte le plus grand nombre de pays répertoriés dans la base de données ?
 
 
 
--- Quel est le nom du pays d'origine de la marque de bière "Heineken" ?
+-- 4/ Quel est le nom du pays d'origine de la marque de bière "Heineken" ?
 
 
 
--- Combien de bières ont été vendues lors de chaque transaction ? Afficher les numéros de ticket, la date de ticket, et le nombre de bières.
+-- 5/ Combien de bières ont été vendues lors de chaque transaction ? Afficher les numéros de ticket, la date de ticket, et le nombre de bières.
 
 
 
--- Quel est le nombre total de bières vendues jusqu'à présent ?
+-- 6/ Quel est le nombre total de bières vendues jusqu'à présent ?
 
 
 
--- Quelle est la marque de bière la plus vendue (en termes de quantité) ?
+-- 7/ Quelle est la marque de bière la plus vendue (en termes de quantité) ?
 
 
