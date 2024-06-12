7ce6b842d6b46c2e8eac3466167614474101eabb
Guillaume Belleuvre <guillaumebelleuvre@gmail.com>
2024-06-12 16:10:34 +0200

    question 1 done

--- sdbm queries 3.sql
+++ sdbm queries 3.sql
@@ -2,6 +2,15 @@
 -- 1/ Donnez la liste des bières de même couleur et de même type que la bière ayant le code 142.
 --     (affichez le code et le nom de la bière, le nom de la couleur et le nom du type)
 
+SELECT id_article, article_name, color_name, type_name
+FROM type
+    JOIN article USING (id_type)
+    JOIN color USING (id_color)
+WHERE (id_color, id_type) = (
+    SELECT id_color, id_type
+    FROM article
+    WHERE id_article = 142
+);
 
 -- 2/ Lister les quantités vendues de chaque article pour les années 2014, 2015,2016 et 2017.
 
