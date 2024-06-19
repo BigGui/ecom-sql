c46d09708fb5323438ceaabe1c0463d46d6be551
Guillaume Belleuvre <guillaumebelleuvre@gmail.com>
2024-06-19 12:13:35 +0200

    sdbm query 6 new type values by year

--- sdbm queries 6.sql
+++ sdbm queries 6.sql
@@ -5,9 +5,26 @@
     -- Si le volume total vendu d'une marque atteint 10 hectolitres, le fabricant gagne 30 points par hectolitre intégralement vendu.
     -- Si le volume total vendu d'une marque atteint 20 hectolitres, le fabricant gagne 40 points par hectolitre intégralement vendu.
 
--- En plus de ces points, les fabricants touchent un bonus de points s'ils ont vendu (toutes marques confondues) des bières de certains types : 
-    -- Plus de 2000 litres de bière de type Trappiste, 300 points ;
-    -- Plus de 1500 litres de bière de type Bière de Saison, 200 points ;
-    -- Plus de 1000 litres de bière de type Stout, 100 points ;
+-- En plus de ces points, les fabricants touchent un bonus de points s'ils ont vendu (toutes marques confondues) des bières de certains types. Tous les ans les types de bière permettant d'obtenir ces points bonus changent. Voici les règles d'attribution de ces points bonus :
+
+    -- En 2014 c'était :
+        -- Pour plus de 300 litres de bière de type Abbaye, 300 points ;
+        -- Pour plus de 200 litres de bière de type Pils et Lager, 200 points ;
+        -- Pour plus de 100 litres de bière de type Stout, 100 points ;
+
+    -- En 2015 c'était :
+        -- Pour plus de 1500 litres de bière de type Abbaye, 300 points ;
+        -- Pour plus de 1000 litres de bière de type Pils et Lager, 200 points ;
+        -- Pour plus de 500 litres de bière de type Lambic, 100 points ;
+
+    -- En 2016 c'était :
+        -- Pour plus de 500 litres de bière de type Trappiste, 300 points ;
+        -- Pour plus de 300 litres de bière de type Bière Aromatisée, 200 points ;
+        -- Pour plus de 200 litres de bière de type Ale, 100 points ;
+
+    -- En 2017, c'était :
+        -- Pour plus de 2000 litres de bière de type Trappiste, 300 points ;
+        -- Pour plus de 1500 litres de bière de type Bière de Saison, 200 points ;
+        -- Pour plus de 1000 litres de bière de type Stout, 100 points ;
 
 -- Calculer le nombre de points de fidélités gagnés par chaque fabricant pour chacune des années écoulées.
